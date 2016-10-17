1d2fd507f8d3ca43873e22c44855befe5274a562
piotrpliszko <pliszko.piotr@gmail.com>
2016-10-17 08:51:46 +0200

    initial backend commit

---
+++ ankieter/src/main/java/pl/tas/AnkieterApplication.java
@@ -0,0 +1,12 @@
+package pl.tas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkieterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkieterApplication.class, args);
+	}
+}
---
+++ ankieter/src/test/java/pl/tas/AnkieterApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnkieterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
